b7b085b500683ac8eefce08b866528453d573115
Srimahitha Kanakamedala <srimahitha.kanakamedala@esums.tech>
2025-02-21 13:43:27 +0000

    adjusted nav bar and images to add black box code

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.hellotime, name="hellotime"),
-    path("screenprint", views.screenprint, name="screenprint")
+    path("screenprint", views.screenprint, name="screenprint"),
+    path("blackbox", views.blackbox, name="blackbox")
 ]
--- core/views.py
+++ core/views.py
@@ -9,4 +9,7 @@ def hellotime(request):
 def screenprint(request):
     return render(request, "core/screenprint.html")
 
+def blackbox(request):
+    return render(request, "core/blackbox.html")
+
 # Create your views here.
